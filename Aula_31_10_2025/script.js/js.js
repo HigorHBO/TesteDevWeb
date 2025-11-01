fa1aaf4371753c9af6429fb066f463466214c6e5
HigorHBO <hbo.higor@gmail.com>
2025-10-31 21:44:26 -0300

    Aula 31/10/2025

---
+++ Aula_31_10_2025/script.js/js.js
@@ -0,0 +1,24 @@
+function getDados(){
+    let elementos = document.getElementsByName('happy')
+    for (let i=0; i < elementos.length; i++){
+        if(elementos[i].checked){
+            document.getElementById('result').innerHTML = elementos[i].value;
+        }
+    }
+
+    let drink =  document.querySelector('#drink');
+    document.getElementById('fd').innerHTML = drink.selected.value;
+}
+
+
+
+
+
+
+
+
+
+
+//function getDados(){
+   // let email = document.getElementById('email').value;
+    //window.alert("Olá o email "+ email +" foi recebido");
